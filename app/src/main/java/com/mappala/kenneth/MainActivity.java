7380b8e122432dae2bdb56200a0265645feeb628
ICS115KEN <2014069382@ust-ics.mygbiz.com>
2018-10-15 16:33:12 +0800

    Initial commit

---
+++ app/src/main/java/com/mappala/kenneth/MainActivity.java
@@ -0,0 +1,28 @@
+package com.mappala.kenneth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    protected Button myButton;
+    String myString = "Added to Cart";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myButton = (Button)findViewById(R.id.button3);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),myString,Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
